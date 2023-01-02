4bd56ad7ecdda1532127f20c199085edd1cc8d47
Rafal <n1troov4@gmail.com>
2023-01-02 14:07:13 +0100

    some basics

---
+++ BlazorWebTab/Server/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebTab.Server.Controllers;
+
+public class ProductController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
