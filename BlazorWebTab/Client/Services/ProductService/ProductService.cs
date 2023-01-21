44eb08b02352d79cd385ca527546b407641671e9
itraf <it.rafal.nowak@gmail.com>
2023-01-21 15:53:51 +0100

    new lines

--- BlazorWebTab/Client/Services/ProductService/ProductService.cs
+++ BlazorWebTab/Client/Services/ProductService/ProductService.cs
@@ -6,7 +6,6 @@ namespace BlazorWebTab.Client.Services.ProductService;
 public class ProductService : IProductService
 {
     private readonly HttpClient _httpClient;
-
     public string Message { get; set; } = "Products loading...";
     
     public event Action? ProductChanged;
@@ -34,7 +33,6 @@ public class ProductService : IProductService
         ProductChanged?.Invoke();
     }
     
-
     public async Task<ServiceResponse<Product>> GetproductById(int productId)
     {
         var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
