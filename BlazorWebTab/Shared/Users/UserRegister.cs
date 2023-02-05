b0d690febfc7e4ec5def7280e38fc091f528ff3e
itraf <it.rafal.nowak@gmail.com>
2023-02-05 21:30:50 +0100

    add register buton on right top menu

---
+++ BlazorWebTab/Shared/Users/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebTab.Shared.Users;
+
+public class UserRegister
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+    
+}
