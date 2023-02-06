86c26903292d0288562cc78cedfdcfbc6ea33d72
itraf <it.rafal.nowak@gmail.com>
2023-02-06 22:35:41 +0100

    lauch settings

--- BlazorWebTab/Shared/Users/UserRegister.cs
+++ BlazorWebTab/Shared/Users/UserRegister.cs
@@ -1,9 +1,15 @@
-﻿namespace BlazorWebTab.Shared.Users;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebTab.Shared.Users;
 
 public class UserRegister
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required, StringLength(10, MinimumLength = 2)]
     public string Password { get; set; } = string.Empty;
+    
+    [Compare("Password", ErrorMessage = "Password do not match")]
     public string ConfirmPassword { get; set; } = string.Empty;
     
 }
