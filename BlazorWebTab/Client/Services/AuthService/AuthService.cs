bf89d07daf3c1325c0baded2b9d31d538fc2fe49
itraf <it.rafal.nowak@gmail.com>
2023-02-09 23:06:43 +0100

    register on client side

--- BlazorWebTab/Client/Services/AuthService/AuthService.cs
+++ BlazorWebTab/Client/Services/AuthService/AuthService.cs
@@ -1,6 +1,20 @@
-﻿namespace BlazorWebTab.Client.Services.AuthService;
+﻿using System.Net.Http.Json;
+using BlazorWebTab.Shared;
+using BlazorWebTab.Shared.Users;
+
+namespace BlazorWebTab.Client.Services.AuthService;
 
 public class AuthService : IAuthService
 {
-    
+    private readonly HttpClient _httpClient;
+
+    public AuthService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<ServiceResponse<int>> Register(UserRegister request)
+    {
+        var responseMessage = await _httpClient.PostAsJsonAsync($"api/register", Register(request));
+        return await responseMessage.Content.ReadFromJsonAsync<ServiceResponse<int>>();;
+    }
 }
--- BlazorWebTab/Client/Services/AuthService/IAuthService.cs
+++ BlazorWebTab/Client/Services/AuthService/IAuthService.cs
@@ -1,6 +1,9 @@
-﻿namespace BlazorWebTab.Client.Services.AuthService;
+﻿using BlazorWebTab.Shared;
+using BlazorWebTab.Shared.Users;
+
+namespace BlazorWebTab.Client.Services.AuthService;
 
 public interface IAuthService
 {
-    
+    public Task<ServiceResponse<int>> Register(UserRegister request);
 }
